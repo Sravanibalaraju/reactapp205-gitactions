d055349380915e8ece9686cc74bfe41586b34f7d
Sravani <sravanishiva192@gmail.com>
2025-07-28 11:25:34 +0530

    updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,8 @@ function App() {
   return (
     <div className="container">
       <h1>Welcome to My React Project - SRAVANI</h1>
-      <h1>welcome to CI/CD DevOps</h1>
       <p>This is a simple homepage deployed on GitHub Pages using Vite.</p>
+      <h1>welcome to CI/CD DevOps</h1>
     </div>
   );
 }
